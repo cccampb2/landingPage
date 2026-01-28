0495a64be209510c79073169f000522f91d6cafe
cccampb2 <calebccampbell19@gmail.com>
2026-01-27 22:23:36 -0500

    hero items positioning

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = ({}) => {
   return (
     <Section name={"hero"}>
       <div className="hero">
-        <div className="hero__intro"></div>
+        <div className="hero__intro">HI, I'M CALEB</div>
         <div className="hero__avatar">
-          <img className="avatar" src={avatar} alt="Caleb-Campbell-Avatar" />
+          <img src={avatar} alt="Caleb-Campbell-Avatar" />
         </div>
       </div>
     </Section>
